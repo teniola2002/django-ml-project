b849422501d7e34c0614f291ea0934865e7b6442
teniola2002 <148721993+teniola2002@users.noreply.github.com>
2026-04-14 20:38:39 +0200

    add input form fields for ML predictions

--- myproject/predictor/views.py
+++ myproject/predictor/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse 
 from django.shortcuts import render
 def home(request):
-    return render(request,"home.html")
+    return render(request, "predictor/home.html")
+
+def result(request):
+    return render(request, "predictor/result.html")
